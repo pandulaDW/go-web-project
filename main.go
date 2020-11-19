3a1e996a3eb12fc2e46c20499666ce3f005b4665
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-11-19 10:10:10 +0530

    Added air library for hot reloading

--- main.go
+++ main.go
@@ -5,19 +5,34 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which writes a byte slice
-// as the response body
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from SnippetBox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on port 4000...")
 	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(err)
 }
