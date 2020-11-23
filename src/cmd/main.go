1b717d2052a303323b6bd558aae382b7d01a2df4
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-11-23 17:13:16 +0530

    Added routes to another file

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/pandulaDW/go-web-project/src/cmd/config"
-	"github.com/pandulaDW/go-web-project/src/cmd/snippets"
 )
 
 func main() {
@@ -20,23 +19,11 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	// create the serve mux router
-	mux := http.NewServeMux()
-
-	// register handlers
-	mux.HandleFunc("/", snippets.Home(&app))
-	mux.HandleFunc("/snippet", snippets.ShowSnippet(&app))
-	mux.HandleFunc("/snippet/create", snippets.CreateSnippet(&app))
-
-	// static file handling
-	fileServer := http.FileServer(http.Dir("./src/ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	// create a new http server struct
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: app.ErrorLogger,
-		Handler:  mux,
+		Handler:  routes(&app),
 	}
 
 	// starting the server
---
+++ src/cmd/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/pandulaDW/go-web-project/src/cmd/config"
+	"github.com/pandulaDW/go-web-project/src/cmd/snippets"
+)
+
+func routes(app *config.Application) *http.ServeMux {
+	// create the serve mux router
+	mux := http.NewServeMux()
+
+	// register handlers
+	mux.HandleFunc("/", snippets.Home(app))
+	mux.HandleFunc("/snippet", snippets.ShowSnippet(app))
+	mux.HandleFunc("/snippet/create", snippets.CreateSnippet(app))
+
+	// static file handling
+	fileServer := http.FileServer(http.Dir("./src/ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
