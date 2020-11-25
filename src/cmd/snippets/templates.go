9bb1f6ccc50d0c523f719232472b83d85e776bb3
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-11-25 17:46:39 +0530

    Added caching for templates

--- src/cmd/snippets/templates.go
+++ src/cmd/snippets/templates.go
@@ -1,8 +1,40 @@
 package snippets
 
-import "github.com/pandulaDW/go-web-project/src/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/pandulaDW/go-web-project/src/pkg/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.htm"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.htm"))
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
